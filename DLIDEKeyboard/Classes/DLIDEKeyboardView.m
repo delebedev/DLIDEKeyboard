3221546c3128d7e8d58b1731886ad78ad564ec29
Denis Lebedev <d2.lebedev@gmail.com>
2013-01-14 19:12:47 +0300

    Add assets

--- DLIDEKeyboard/Classes/DLIDEKeyboardView.m
+++ DLIDEKeyboard/Classes/DLIDEKeyboardView.m
@@ -10,7 +10,12 @@
 
 #define IS_IPAD UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
 
-@interface DLIDEKeyboardView () {
+NSString *const kHideKeyBoardKey = @"k";
+NSString *const kTabKey = @"\u279D";
+NSString *const kFirstRowKey = @"\u25D0";
+NSString *const kSecondRowKey = @"\u25D1";
+
+@interface DLIDEKeyboardView () <UIInputViewAudioFeedback> {
     NSArray *_keys;
 }
 
@@ -36,6 +41,17 @@
         CGFloat kKeyboardHeight = IS_IPAD ? 104.f : 34.f;
         self.frame = CGRectMake(0, 0, kKeyboardWidth, kKeyboardHeight);
 
+        
+        UIView *border1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kKeyboardWidth, 1)];
+        border1.backgroundColor = [UIColor colorWithRed:51/255. green:51/255. blue:51/255. alpha:1.];
+        border1.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+        [self addSubview:border1];
+        
+        UIView *border2 = [[UIView alloc] initWithFrame:CGRectMake(0, 1, kKeyboardWidth, 1)];
+        border2.backgroundColor = [UIColor colorWithRed:191/255. green:191/255. blue:191/255. alpha:1.];
+        border2.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+        [self addSubview:border2];
+        
         self.backgroundColor = IS_IPAD ? [UIColor colorWithRed:156/255. green:155/255. blue:166/255. alpha:1.] :
         [UIColor colorWithRed:125/255.f green:132/255.f blue:146/255.f alpha:1.f];
         self.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
@@ -43,19 +59,19 @@
         NSInteger kRows = IS_IPAD ? 2 : 1;
         NSInteger kKeysInRow = IS_IPAD ? 14 : 10;
         CGFloat kLeftPadding = 3.f;
-        CGFloat kTopPadding = 3.f;
+        CGFloat kTopPadding = 4.f;
         CGFloat kSpacing =  IS_IPAD ? 5.f : 2.f;
         NSInteger kButtonWidth = (kKeyboardWidth - 2 * kLeftPadding - (kKeysInRow - 1) * kSpacing) / kKeysInRow;
         kLeftPadding = (kKeyboardWidth - kButtonWidth * kKeysInRow - kSpacing * (kKeysInRow - 1)) / 2;
                 
         if (IS_IPAD) {
             _keys = @[
-            @[@"->", @"{", @"}", @"[", @"]", @"(", @")", @">",@"<", @"@", @"'", @"\"", @":", @"_"],
+            @[kTabKey, @"{", @"}", @"[", @"]", @"(", @")", @">",@"<", @"@", @"'", @"\"", @":", @"_"],
             @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"0", @"+", @"-", @"/", @"*"]];
         } else {
             _keys = @[
-            @[@"k", @"{", @"}", @"[", @"]", @"(", @")", @">",@"<", @".."],
-            @[@"+", @"-", @"/", @"*",@"=", @"@", @"\"", @":", @"_", @".."]];
+            @[kHideKeyBoardKey, @"+", @"-", @"/", @"*",@"=", @"@", @"\"", @":", kFirstRowKey],
+            @[@"{", @"}", @"[", @"]", @"(", @")", @">",@"<", @"_", kSecondRowKey]];
         }
         
         for (int i = 0; i < kRows; i++) {
@@ -65,7 +81,12 @@
                 [b addTarget:self action:@selector(handleTap:) forControlEvents:UIControlEventTouchUpInside];
                 [b setTitle:_keys[i][j] forState:UIControlStateNormal];
                 [b setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-                [b setBackgroundImage:[UIImage imageNamed:@"key"] forState:UIControlStateNormal];
+                if (_keys[i][j] == kSecondRowKey || _keys[i][j] == kFirstRowKey) {
+                    [b setBackgroundImage:[UIImage imageNamed:@"key-blue"] forState:UIControlStateNormal];
+                } else {
+                    [b setBackgroundImage:[UIImage imageNamed:@"key"] forState:UIControlStateNormal];
+                }
+                [b setImage:_keys[i][j] == kHideKeyBoardKey ? [UIImage imageNamed:@"keyboard"] : nil forState:UIControlStateNormal];
                 [b setBackgroundImage:[UIImage imageNamed:@"key-pressed"] forState:UIControlStateHighlighted];
                 b.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
                 b.frame = CGRectMake(kLeftPadding + j*(kButtonWidth + kSpacing), kTopPadding + i*kButtonWidth, kButtonWidth, kButtonWidth);
@@ -76,25 +97,39 @@
     return self;
 }
 
+#pragma mark - Private
+
 - (void)handleTap:(UIButton *)button {
+    [[UIDevice currentDevice] playInputClick];
     NSString *input = button.titleLabel.text;
-    if ([input isEqualToString:@"k"]) {
+    if ([input isEqualToString:kHideKeyBoardKey]) {
         [(id)self.textView resignFirstResponder];
         return;
     }
-    if ([input isEqualToString:@".."]) {
+    if ([input isEqualToString:kTabKey]) {
+        [self.textView insertText:@"  "];
+    }
+    if ([input isEqualToString:kFirstRowKey] || [input isEqualToString:kSecondRowKey]) {
         self.selectedRow = self.selectedRow == 1 ? 0 : 1;
         [self refreshButtons];
         return;
     }
+    
     [self.textView insertText:input];
 }
 
 - (void)refreshButtons {
     for (int j = 0; j < 10; j++) {
         UIButton *b = (UIButton *)[self viewWithTag:j + 100];
+        [b setImage:nil forState:UIControlStateNormal];
         [b setTitle:_keys[self.selectedRow][j] forState:UIControlStateNormal];
     }
+}
+
+#pragma mark - UIInputViewAudioFeedback
+
+- (BOOL)enableInputClicksWhenVisible {
+    return YES;
 }
 
 
